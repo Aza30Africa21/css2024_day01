69a0b78deb384abeef53d4e3dfc5ccc3ad8746d1
Aza30Africa21 <157531215+Aza30Africa21@users.noreply.github.com>
2024-01-30 10:36:30 +0200

    Add files via upload

---
+++ day_file2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 09:54:44 2024
+
+@author: Mokgadi Precious Mph
+"""
+
+import pandas as pd
+
+file = pd.read_csv("data_02/iris.csv")
+
+"""
+Absolute Path:
+
+C:/Users/Mokgadi Precious Mph/css2024_day02/data_02/data_02/iris.csv
+
+Relative Path:
+data_02/iris.csv
+"""
+
+#file = pd.read
+
